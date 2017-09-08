ab4612788761c58fb9d6bc149747443c2a265de8
N.C Wu <mythnc@gmail.com>
2017-09-08 10:42:55 +0800

    Add exercise 3 - printing quotes

---
+++ ex3-printing-quotes/ex3-printing-quotes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3_printing_quotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the quote? ");
+            var quote = Console.ReadLine();
+            Console.Write("Who said it? ");
+            var author = Console.ReadLine();
+
+            Console.WriteLine(author + " says, " + "\"" + quote + "\"");
+
+            Console.ReadKey();
+        }
+    }
+}
