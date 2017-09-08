58a6c438be369c1355c3889a0e8f3a5bd24706c0
N.C Wu <mythnc@gmail.com>
2017-09-08 09:32:46 +0800

    Add exercise 1 - saying hello

---
+++ ex1-saying-hello/ex1-saying-hello/ex1-saying-hello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex1_saying_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.Write("Hello, " + name + ", nice to meet you!");
+            Console.ReadKey();
+        }
+    }
+}
