e7ec33dc456abf6ccb772cd42a2c2d99edd8fb8d
N.C Wu <mythnc@gmail.com>
2017-09-08 10:09:36 +0800

    Add exercise 2 - counting the number of characters

---
+++ ex2-counting-the-number-of-characters/ex2-counting-the-number-of-characters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2_counting_the_number_of_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = GetInputString();
+            while (input.Length == 0)
+            {
+                Console.WriteLine("You must enter something");
+                input = GetInputString();
+            }
+
+            var len = input.Length;
+            Console.Write("{0} has {1} character", input, len);
+            if (len > 1)
+            {
+                Console.Write("s");
+            }
+            Console.WriteLine(".");
+
+            Console.ReadKey();
+        }
+
+        static String GetInputString()
+        {
+            Console.Write("What is the input string? ");
+            return Console.ReadLine();
+        }
+    }
+}
